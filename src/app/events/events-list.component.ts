d821662fbc62b06f278d5fcf3842cd6dc5619a30
bfraczak <bartek201@gmail.com>
2021-01-03 17:47:01 +0100

    event thumbnail

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -6,22 +6,11 @@ import {Component} from "@angular/core";
     <div>
       <h1>Upcoming Angular events</h1>
       <hr>
-      <div class="well hoverwell thumbnail">
-        <h2>{{event.name}}</h2>
-        <div>Date: {{event.date}}</div>
-        <div>Time: {{event.time}}</div>
-        <div>Price: \${{event.price}}</div>
-        <div>
-          <span>Location: {{event.location.address}}</span>
-          <span>&nbsp;</span>
-          <span>{{event.location.city}}, {{event.location.country}}</span>
-        </div>
-      </div>
-    </div>
-  `
+      <event-thumbnail #thumbnail [event]="event1"></event-thumbnail>
+    </div>`
 })
 export class EventsListComponent {
-  event = {
+  event1 = {
     id: 1,
     name: 'Angular Connect',
     date: '9/26/2023',
