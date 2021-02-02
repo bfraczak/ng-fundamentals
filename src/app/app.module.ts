89f88c1a34ba6e5651ce642c1301f1954318aba8
bfraczak <bartek201@gmail.com>
2021-02-02 22:45:39 +0100

    toastr service injection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,13 +14,15 @@ import {
 
 import { EventsAppComponent } from './events-app.component';
 import {NavbarComponent} from "./nav/navbar.component";
-import {ToastrService} from "./common/toastr.service";
+import {Toastr, TOASTR_TOKEN} from "./common/toastr.service";
 import {RouterModule} from "@angular/router";
 import {appRoutes} from "./routes";
 import {Error404Component} from "./errors/404.component";
 import {AuthService} from "./user/auth.service";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {CollapsibleWellComponent} from "./common/collapsible-well.component";
+
+declare let toastr: Toastr;
 
 @NgModule({
   declarations: [
@@ -44,12 +46,16 @@ import {CollapsibleWellComponent} from "./common/collapsible-well.component";
   ],
   providers: [
     EventService,
-    ToastrService,
+    {
+      provide: TOASTR_TOKEN,
+      useValue: toastr
+    },
     EventRouteActivatorService,
     EventsListResolverService,
     {
       provide: 'canDeactivateCreateEvent',
-      useValue: checkDirtyState},
+      useValue: checkDirtyState
+    },
     AuthService
   ],
   bootstrap: [EventsAppComponent]
--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit} from "@angular/core";
 import {EventService} from "./shared/event.service";
-import {ToastrService} from "../common/toastr.service";
 import {ActivatedRoute} from "@angular/router";
 import {IEvent} from "./shared";
 
@@ -11,21 +10,17 @@ import {IEvent} from "./shared";
       <hr>
       <div class="row">
         <div class="col-md-5" *ngFor="let event of events" >
-            <event-thumbnail (click)="handleThumbnailClick(event.name)" [event]="event"></event-thumbnail>
+            <event-thumbnail [event]="event"></event-thumbnail>
         </div>
       </div>
     </div>`
 })
 export class EventsListComponent implements OnInit{
   events: IEvent[] | undefined;
-  constructor(private eventService: EventService, private toastr: ToastrService, private route: ActivatedRoute) {};
+  constructor(private eventService: EventService, private route: ActivatedRoute) {};
 
 
   ngOnInit(){
     this.events = this.route.snapshot.data['events'];
-  }
-
-  handleThumbnailClick(name: any) {
-    this.toastr.success(name);
   }
 }
